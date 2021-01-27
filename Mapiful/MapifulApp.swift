b5e0b2506cd3f19e6ce3da64d64dbb29a20dd528
Peter Larson <p.larson@utexas.edu>
2021-01-26 19:23:17 -0600

    Initial Commit

---
+++ Mapiful/MapifulApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapifulApp.swift
+//  Mapiful
+//
+//  Created by Peter Larson on 1/26/21.
+//
+
+import SwiftUI
+
+@main
+struct MapifulApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
